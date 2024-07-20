f1dbea1a7cd5693457c285e35c848b453a09de5f
Rahul Raj <rahul.r0644@gmail.com>
2024-07-20 15:00:43 +0530

    commit

--- student-portal/src/components/Main.jsx
+++ student-portal/src/components/Main.jsx
@@ -5,14 +5,41 @@ function Main() {
   return (
     <main>
         <section class="semesters">
-            <div class="semester">Semester 1</div>
-            <div class="semester">Semester 2</div>
-            <div class="semester">Semester 3</div>
-            <div class="semester">Semester 4</div>
-            <div class="semester">Semester 5</div>
-            <div class="semester">Semester 6</div>
-            <div class="semester">Semester 7</div>
-            <div class="semester">Semester 8</div>
+            <div class="semester">CHE110</div>
+            <div class="semester">CSE111</div>
+            <div class="semester">CSE326</div>
+            <div class="semester">ECE249</div>
+            <div class="semester">ECE279</div>
+            <div class="semester">INT108</div>
+            <div class="semester">MTH174</div>
+            <div class="semester">PES318</div>
+
+            <div class="semester">CSE101</div>
+            <div class="semester">CSE121</div>
+            <div class="semester">CSE320</div>
+            <div class="semester">INT306</div>
+            <div class="semester">MEC135</div>
+            <div class="semester">PHY110</div>
+            <div class="semester">MTH401</div>
+            <div class="semester">PEL130</div>
+
+            <div class="semester">CSE202</div>
+            <div class="semester">CSE205</div>
+            <div class="semester">CSE211</div>
+            <div class="semester">CSE306</div>
+            <div class="semester">CSE307</div>
+            <div class="semester">PEL134</div>
+            <div class="semester">MTH302</div>
+
+            <div class="semester">CSE310</div>
+            <div class="semester">CSE316</div>
+            <div class="semester">CSE325</div>
+            <div class="semester">CSE408</div>
+            <div class="semester">INT330</div>
+            <div class="semester">INT362</div>
+            <div class="semester">INT426</div>
+            <div class="semester">PEA305</div>
+            
         </section>
     </main>
   );
